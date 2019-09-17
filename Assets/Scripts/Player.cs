f07ca3bac4d6d44f6ab285b57727a0328c52769a
TheGamingshame <Leon932002@gmail.com>
2019-09-17 18:56:14 +0200

    Player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int speed;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        movement();
     }
+    
+    public void movement()
+    {
+        Vector3 movement = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed); 
+        transform.position = transform.position + movement * Time.deltaTime;
+    }
+    
 }
+
